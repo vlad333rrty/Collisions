87c06a81349ae4186de4826047f4b9d610b379ae
vlad333rrty <vlad333rrty90@mail.ru>
2019-09-09 01:03:30 +0300

    AABB-Circle collision detection improved,code rearrangement

--- src/Hitbox.java
+++ src/Hitbox.java
@@ -3,14 +3,13 @@ abstract class Hitbox {
     Hitbox(Vector2d centre){
         this.centre=centre;
     }
-    abstract Vector2d getCollision(Hitbox object);
     abstract void changePosition(Vector2d newPosition);
     abstract void correctPosition(Vector2d distance);
 }
 
 class AABB extends Hitbox{
-    private Vector2d[] vertices;
-    private Vector2d firstAxis,secondAxis;
+    Vector2d[] vertices;
+    Vector2d firstAxis,secondAxis;
 
     /**
      * Constructor receives a centre and an array
@@ -25,97 +24,6 @@ class AABB extends Hitbox{
         secondAxis=secondSide.normal().normalize();
     }
 
-    private Pair<Double,Double> minMaxProjection(AABB object,Vector2d axis){
-        double minPr,maxPr,projection;
-        minPr=maxPr=object.vertices[0].vectorProjection(axis);
-        for (int i=1;i<4;i++){
-            projection=object.vertices[i].vectorProjection(axis);
-            if (projection>maxPr){
-                maxPr=projection;
-            }else if (projection<minPr){
-                minPr=projection;
-            }
-        }
-        return new Pair<>(minPr,maxPr);
-    }
-
-    private double overlap(Pair<Double,Double> pr1,Pair<Double,Double> pr2){
-        double min,max;
-        if (pr1.second<pr2.second){
-            max=pr1.second;
-            min=pr2.first;
-        }else {
-            max=pr2.second;
-            min=pr1.first;
-        }
-        return min-max;
-    }
-
-    private Pair<Double,Vector2d> getMin(double dx,double dy,Vector2d firstAxis,Vector2d secondAxis){
-        double depth;
-        Vector2d axis;
-        if (dx<dy){
-            depth=dy;
-            axis=new Vector2d(secondAxis);
-        }else {
-            depth=dx;
-            axis=new Vector2d(firstAxis);
-        }
-        return new Pair<>(depth,axis);
-    }
-
-    /**
-     * Returns vector every point of the polygon
-     * will have been translated along
-     */
-    @Override
-    Vector2d getCollision(Hitbox object) {
-        if (object instanceof AABB){
-            AABB obj=(AABB)object;
-            Pair<Double,Double> thisPr1=minMaxProjection(this,firstAxis);
-            Pair<Double,Double> objPr1=minMaxProjection(obj,firstAxis);
-            Pair<Double,Double> thisPr2=minMaxProjection(this,secondAxis);
-            Pair<Double,Double> objPr2=minMaxProjection(obj,secondAxis);
-
-            Pair<Double,Double> secThisPr1=minMaxProjection(this,obj.firstAxis);
-            Pair<Double,Double> secObjPr1=minMaxProjection(obj,obj.firstAxis);
-            Pair<Double,Double> secThisPr2=minMaxProjection(this,obj.secondAxis);
-            Pair<Double,Double> secObjPr2=minMaxProjection(obj,obj.secondAxis);
-
-            double dx,dy,dX,dY;
-
-            if ((dx=overlap(thisPr1,objPr1))<0 && (dy=overlap(thisPr2,objPr2))<0
-                    && (dX=overlap(secThisPr1,secObjPr1))<0 && (dY=overlap(secThisPr2,secObjPr2))<0){
-
-                Pair<Double,Vector2d> thisMin=getMin(dx,dy,firstAxis,secondAxis);
-                Pair<Double,Vector2d> objMin=getMin(dX,dY,obj.firstAxis,obj.secondAxis);
-                Pair<Double,Vector2d> result=getMin(thisMin.first,objMin.first,thisMin.second,objMin.second);
-
-                Vector2d centre_to_centre=centre.sub(obj.centre,new Vector2d());
-                if (centre_to_centre.vectorProjection(result.second)>0) result.second.mul(-1.0);
-                return new Vector2d(result.second.mul(result.first));
-            }
-        }
-        if (object instanceof CircleHitbox){
-            CircleHitbox obj=(CircleHitbox)object;
-
-            Vector2d axis=obj.centre.sub(centre,new Vector2d());
-            Pair<Double,Double> thisPr=minMaxProjection(this,axis);
-
-            double centreProjection=obj.centre.vectorProjection(axis);
-            Pair<Double,Double> struct=new Pair<>(centreProjection-obj.radius,centreProjection+obj.radius);
-
-            double depth;
-            if ((depth=overlap(thisPr,struct))<0){
-                return new Vector2d(axis.normalize().mul(depth));
-            }
-        }
-        return null;
-    }
-
-	/**
-	 * Changes the postion of the hitbox
-	 */
     @Override
     void changePosition(Vector2d newPosition) {
         Vector2d delta=newPosition.sub(centre,new Vector2d());
@@ -124,10 +32,7 @@ class AABB extends Hitbox{
             vertices[i].add(delta);
         }
     }
-    
-	/**
-	 * Must be called if a collision occurred
-	 */
+
     @Override
     void correctPosition(Vector2d distance) {
         changePosition(centre.add(distance,new Vector2d()));
@@ -141,45 +46,16 @@ class CircleHitbox extends Hitbox{
         this.radius=radius;
     }
 
-    /**
-     * Returns vector every point of the polygon
-     * will have been translated along
-     */
-    @Override
-    Vector2d getCollision(Hitbox object) {
-        if (object instanceof AABB){
-            AABB obj=(AABB)object;
-            Vector2d depth=obj.getCollision(this);
-            if (depth!=null){
-                return depth.mul(-1.0);
-            }
-        }else if (object instanceof CircleHitbox){
-            CircleHitbox obj=(CircleHitbox) object;
-
-            Vector2d axis=obj.centre.sub(centre,new Vector2d());
-            double dist=axis.length();
-            if (dist<radius+obj.radius){
-                return new Vector2d(axis.normalize().mul(radius+obj.radius-dist));
-            }
-        }
-        return null;
-    }
-	
-	/**
-	 * Changes the postion of the hitbox
-	 */
     @Override
     void changePosition(Vector2d newPosition) {
         centre.set(newPosition);
     }
-	
-	/**
-	 * Must be called if a collision occurred
-	 */
+
     @Override
     void correctPosition(Vector2d distance) {
         changePosition(centre.add(distance,new Vector2d()));
     }
+
 }
 
 class Pair<F,S>{
@@ -188,5 +64,176 @@ class Pair<F,S>{
     Pair(F f,S s){
         first=f;
         second=s;
+    }
+}
+
+class Intersection{
+    private static Vector2d gramSchmidt(Vector2d axis,Vector2d vector){
+        return vector.sub(axis.mul(vector.dotProduct(axis)/axis.dotProduct(axis)),new Vector2d());
+    }
+    
+    private static Vector2d intersect(Vector2d A,Vector2d B,Vector2d C,Vector2d D){
+        double x1=B.x,x0=A.x,y1=B.y,y0=A.y;
+        double a=C.x,b=C.y,c=D.x,d=D.y;
+        double l=c-a,q=d-b,n=x1-x0,m=y1-y0;
+        double t=(n*(y0-b)+m*(a-x0))/(q*n-m*l);
+        double x=a+t*l,y=b+t*q;
+        return new Vector2d(x,y);
+    }
+    
+    private static Vector2d determineLine(Vector2d orthogonal,Vector2d point){
+        if (orthogonal.x==0){
+            return new Vector2d(point.x,0);
+        }
+        if (orthogonal.y==0){
+            return new Vector2d(0,point.y);
+        }
+        return new Vector2d(0,-orthogonal.y*point.x/orthogonal.x+point.y);
+    }
+    
+    private static Vector2d calcLine(Vector2d a,Vector2d b,Vector2d point){
+        Vector2d vector=b.sub(a,new Vector2d());
+        Vector2d orthogonal=gramSchmidt(vector,point.sub(a,new Vector2d()));
+        Vector2d inter=intersect(a,b,point,determineLine(orthogonal,point));
+        double minX,minY,maxX,maxY;
+        if (a.x<b.x){
+            maxX=b.x;
+            minX=a.x;
+        }else {
+            maxX=a.x;
+            minX=b.x;
+        }
+        if (a.y<b.y){
+            maxY=b.y;
+            minY=a.y;
+        }else {
+            maxY=a.y;
+            minY=b.y;
+        }
+        if (minX<=inter.x && inter.x<=maxX && inter.y<=maxY && inter.y>=minY){
+            return inter;
+        }else {
+            Vector2d q=point.sub(a,new Vector2d());
+            Vector2d l=point.sub(b,new Vector2d());
+            if (q.lengthSquared()<l.lengthSquared()){
+                return a;
+            }else {
+                return b;
+            }
+        }
+    }
+    
+    static Vector2d findClosest(AABB box,Vector2d dot){
+        Vector2d inter=calcLine(box.vertices[0],box.vertices[1],dot);
+        Vector2d guid=inter.sub(dot,new Vector2d());
+        int n=box.vertices.length;
+        for (int i=1;i<n;i++) {
+            Vector2d interNext = calcLine(box.vertices[i%n], box.vertices[(i + 1) % n], dot);
+            Vector2d guidNext=interNext.sub(dot,new Vector2d());
+            if (guid.lengthSquared()>guidNext.lengthSquared()){
+                inter=interNext;
+                guid=guidNext;
+            }
+        }
+        return inter;
+    }
+}
+
+class CollisionSolver{
+    private static Pair<Double,Double> minMaxProjection(AABB object,Vector2d axis){
+        double minPr,maxPr,projection;
+        minPr=maxPr=object.vertices[0].vectorProjection(axis);
+        for (int i=1;i<4;i++){
+            projection=object.vertices[i].vectorProjection(axis);
+            if (projection>maxPr){
+                maxPr=projection;
+            }else if (projection<minPr){
+                minPr=projection;
+            }
+        }
+        return new Pair<>(minPr,maxPr);
+    }
+
+    private static double overlap(Pair<Double,Double> pr1,Pair<Double,Double> pr2){
+        if (pr1.second<pr2.second){
+            return pr2.first-pr1.second;
+        }else {
+            return  pr1.first-pr2.second;
+        }
+    }
+
+    private static Pair<Double,Vector2d> getMin(double dx,double dy,Vector2d firstAxis,Vector2d secondAxis){
+        double depth;
+        Vector2d axis;
+        if (dx<dy){
+            depth=dy;
+            axis=new Vector2d(secondAxis);
+        }else {
+            depth=dx;
+            axis=new Vector2d(firstAxis);
+        }
+        return new Pair<>(depth,axis);
+    }
+
+    private static Vector2d getCollision(AABB obj1,AABB obj2){
+        Pair<Double,Double> thisPr1=minMaxProjection(obj1,obj1.firstAxis);
+        Pair<Double,Double> objPr1=CollisionSolver.minMaxProjection(obj2,obj1.firstAxis);
+        Pair<Double,Double> thisPr2=CollisionSolver.minMaxProjection(obj1,obj1.secondAxis);
+        Pair<Double,Double> objPr2=CollisionSolver.minMaxProjection(obj2,obj1.secondAxis);
+
+        Pair<Double,Double> secThisPr1=CollisionSolver.minMaxProjection(obj1,obj2.firstAxis);
+        Pair<Double,Double> secObjPr1=CollisionSolver.minMaxProjection(obj2,obj2.firstAxis);
+        Pair<Double,Double> secThisPr2=CollisionSolver.minMaxProjection(obj1,obj2.secondAxis);
+        Pair<Double,Double> secObjPr2=CollisionSolver.minMaxProjection(obj2,obj2.secondAxis);
+
+        double dx,dy,dX,dY;
+
+        if ((dx=overlap(thisPr1,objPr1))<0 && (dy=overlap(thisPr2,objPr2))<0
+                && (dX=overlap(secThisPr1,secObjPr1))<0 && (dY=overlap(secThisPr2,secObjPr2))<0){
+
+            Pair<Double,Vector2d> thisMin=CollisionSolver.getMin(dx,dy,obj1.firstAxis,obj1.secondAxis);
+            Pair<Double,Vector2d> objMin=CollisionSolver.getMin(dX,dY,obj2.firstAxis,obj2.secondAxis);
+            Pair<Double,Vector2d> result=CollisionSolver.getMin(thisMin.first,objMin.first,thisMin.second,objMin.second);
+
+            Vector2d centre_to_centre=obj1.centre.sub(obj2.centre,new Vector2d());
+            if (centre_to_centre.vectorProjection(result.second)>0) result.second.mul(-1.0);
+            return new Vector2d(result.second.mul(result.first));
+        }
+        return null;
+    }
+
+    private static Vector2d getCollision(AABB aabb,CircleHitbox circle){
+
+        Vector2d intersectionPoint=Intersection.findClosest(aabb,circle.centre);
+        Vector2d axis=intersectionPoint.sub(circle.centre,new Vector2d());
+        double distance= circle.radius-axis.length();
+        if (distance>0)
+            return axis.normalize().mul(distance);
+        return null;
+    }
+
+    private static Vector2d getCollision(CircleHitbox c1,CircleHitbox c2){
+        Vector2d axis=c2.centre.sub(c1.centre,new Vector2d());
+        double dist=axis.length();
+        if (dist<c1.radius+c2.radius){
+            return new Vector2d(axis.normalize().mul(c1.radius+c2.radius-dist));
+        }
+        return null;
+    }
+    static Vector2d getCollision(Hitbox h1,Hitbox h2){
+        if (h1 instanceof AABB){
+            if (h2 instanceof AABB) return getCollision((AABB)h1,(AABB)h2);
+            if (h2 instanceof CircleHitbox) return getCollision((AABB)h1,(CircleHitbox)h2);
+        }else if (h1 instanceof CircleHitbox){
+            if (h2 instanceof CircleHitbox) return getCollision((CircleHitbox)h1,(CircleHitbox)h2);
+            if (h2 instanceof AABB){
+                Vector2d collision=getCollision((AABB)h2,(CircleHitbox)h1);
+                if (collision != null) {
+                    collision.mul(-1.0);
+                }
+                return collision;
+            }
+        }
+        return null;
     }
 }
